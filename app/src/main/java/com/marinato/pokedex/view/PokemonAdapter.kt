c1eb55c52fdfc0f7d04eeb502635067c62c39375
paulomarinato <marinatopaulo@gmail.com>
2023-06-02 00:43:02 -0300

    estruturando a View 2

--- app/src/main/java/com/marinato/pokedex/view/PokemonAdapter.kt
+++ app/src/main/java/com/marinato/pokedex/view/PokemonAdapter.kt
@@ -4,6 +4,8 @@ package com.marinato.pokedex.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.marinato.pokedex.R
 import com.marinato.pokedex.domain.Pokemon
@@ -22,12 +24,27 @@ class PokemonAdapter(
         val item = items[position]
 
         holder.bindView(item)
-
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bindView(item: Pokemon){
-            this.by
+        fun bindView(item: Pokemon) = with(itemView) {
+            val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+            val tvNumber = findViewById<TextView>(R.id.tvNumber)
+            val tvName = findViewById<TextView>(R.id.tvName)
+            val tvType1 = findViewById<TextView>(R.id.tvType1)
+            val tvType2 = findViewById<TextView>(R.id.tvType2)
+
+            // TODO: Load image with Glade
+
+            tvNumber.text = "N.º ${item.number}"
+            tvName.text = item.name
+            tvType1.text = item.types[0].nome
+
+            if (item.types.size > 1){
+                tvType2.text = item.types[1].nome
+            } else {
+                 
+            }
         }
     }
 }
